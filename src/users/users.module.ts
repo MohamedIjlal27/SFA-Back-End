e5a09147249bf59f66ff7f4fc3650a00b7a1769b
MohamedIjlal00 <mohamed.ijlal@shiftgroup.ca>
2025-07-12 14:30:25 +0530

    feat: send SMS with user credentials on account creation using Twilio

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,9 +4,10 @@ import { UsersService } from './users.service';
 import { UserIdGenerator } from './user-id-generator.util';
 import { PasswordGenerator } from './password-generator.util';
 import { DatabaseModule } from '../database/database.module';
+import { SmsModule } from '../sms/sms.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, SmsModule],
   controllers: [UsersController],
   providers: [UsersService, UserIdGenerator, PasswordGenerator],
   exports: [UsersService],
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,6 +3,7 @@ import { PrismaService } from '../database/prisma.service';
 import { CreateUserDto, UpdateUserDto, UserResponseDto } from '../common/dto/user.dto';
 import { UserIdGenerator } from './user-id-generator.util';
 import { PasswordGenerator } from './password-generator.util';
+import { SmsService } from '../sms/sms.service';
 import * as bcrypt from 'bcrypt';
 
 @Injectable()
@@ -11,6 +12,7 @@ export class UsersService {
     private prisma: PrismaService,
     private userIdGenerator: UserIdGenerator,
     private passwordGenerator: PasswordGenerator,
+    private smsService: SmsService,
   ) {}
 
   async create(createUserDto: CreateUserDto): Promise<UserResponseDto> {
@@ -93,6 +95,17 @@ export class UsersService {
     const response = this.mapToResponseDto(user);
     if (generatedPassword) {
       response.generatedPassword = generatedPassword;
+    }
+
+    // Send SMS with credentials if phone is provided
+    if (user.phone && generatedPassword) {
+      const smsBody = `Welcome to Smartix!\nUser ID: ${user.exeId}\nPassword: ${generatedPassword}\nCompany ID: ${user.companyId}`;
+      try {
+        await this.smsService.sendSms(user.phone, smsBody);
+      } catch (err) {
+        // Log but do not block user creation
+        console.error('Failed to send SMS:', err);
+      }
     }
 
     return response;
